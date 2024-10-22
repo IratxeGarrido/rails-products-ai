034ec1f74374c5c87fe838f284b35acb3cb139da
Iratxe Garrido <iratxe.ggarrido@gmail.com>
2024-10-22 14:19:41 +0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -10,6 +11,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  resources :questions, only: [:index, :create]
 
   # Defines the root path route ("/")
   # root "posts#index"
